47f1460cf57fe48f140f0f8e89da6f874a3ca535
Thant Ko Zaw <thantmkou10@gmail.com>
2022-10-02 18:42:51 +0630

    Add middleware for verifying token

---
+++ middlewares/verifyToken.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userModel');
+
+const verifyToken = (req, res, next) => {
+  const bearer = req.headers['authorization'];
+
+  if (!bearer) {
+    return res.status(401).json({ message: 'Not authenticated' });
+  }
+
+  const [, token] = bearer.split(' ');
+
+  jwt.verify(token, process.env.TOKEN_SERCRET, (err, decoded) => {
+    if (err || !decoded) {
+      return res.status(400).json({ message: 'Not Authorized.' });
+    }
+
+    const userId = decoded.userId;
+
+    User.findById(id, {}, (err, user) => {
+      if (err) next(err);
+
+      if (!user) {
+        return res.status(401).json({ message: 'Not authorized.' });
+      }
+
+      next();
+    });
+  });
+};
+
+module.exports = verifyToken;
