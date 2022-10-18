6f7788db7aa7ad581c0db88a1740864cd4cc52e2
Thant Ko Zaw <thantmkou10@gmail.com>
2022-10-18 18:56:16 +0630

    Set false for virtual id getter

--- models/userModel.js
+++ models/userModel.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
-const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  firstName: { type: String, required: true, minlength: 2 },
-  lastName: { type: String, required: true, minlength: 2 },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    firstName: { type: String, required: true, minlength: 2 },
+    lastName: { type: String, required: true, minlength: 2 },
+  },
+  { id: false },
+);
 
 UserSchema.virtual('fullname').get(function () {
   return `${this.firstName} ${this.lastName}`;
